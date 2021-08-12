844b3e3a3f36cc727e920ac9e0a17fc90f03d9a0
sheryar janjua <u1654095@unimail.co.uk>
2021-08-12 15:00:19 +0100

    added method to increment hopper count

--- src/Home.java
+++ src/Home.java
@@ -2,16 +2,17 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Home extends JFrame {
+public class Home extends JFrame{
     private JPanel panel1;
     private JButton producerMachinesButton;
     private JLabel hopperCountNumField;
     private JButton packagingMachinesButton;
-
+    private JButton button1;
+    private Hopper hopper = new Hopper();
 
     public Home(String title){
         super(title);
-
+//        hopperCountNumField.setText(String.valueOf(hopper.count));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(panel1);
         this.pack();
@@ -22,6 +23,7 @@ public class Home extends JFrame {
                 frame2.setVisible(true);
             }
         });
+
         packagingMachinesButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -29,10 +31,22 @@ public class Home extends JFrame {
                 frame3.setVisible(true);
             }
         });
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hopper.add();
+                System.out.println(hopper.count);
+                hopperCountNumField.setText(String.valueOf(hopper.count));
+            }
+        });
     }
 
     public static void main(String[] args) {
         JFrame frame = new Home("Home");
         frame.setVisible(true);
+        Hopper hopper = new Hopper();
+
     }
+
+
 }
