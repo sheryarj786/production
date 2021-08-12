21ea7f3a6e2a72d6e9b252bc6f927df1968ba17a
sheryar janjua <u1654095@unimail.co.uk>
2021-08-12 09:27:46 +0100

    Creating Main GUI and classes

--- src/Home.java
+++ src/Home.java
@@ -1,2 +1,19 @@
-public class Home {
+import javax.swing.*;
+
+public class Home extends JFrame {
+    private JPanel panel1;
+    private JButton producerMachinesButton;
+
+    public Home(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new Home("Home");
+        frame.setVisible(true);
+    }
 }
