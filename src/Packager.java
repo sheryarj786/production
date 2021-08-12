c63e747fa681fffdfd544760524f6e9900cf2eb0
sheryar janjua <u1654095@unimail.co.uk>
2021-08-12 09:55:49 +0100

    creating packager and producer GUIS

--- src/Packager.java
+++ src/Packager.java
@@ -1,5 +1,22 @@
 import javax.swing.*;
 
-public class Packager {
+public class Packager extends JFrame{
     private JPanel panel1;
+    private JComboBox comboBox1;
+    private JButton viewMachineButton;
+    private JButton removeMachineButton;
+    private JButton addNewMachineButton;
+
+    public Packager(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new Packager("Packager");
+        frame.setVisible(true);
+    }
 }
--- src/Producer.java
+++ src/Producer.java
@@ -1,2 +1,22 @@
-public class Producer {
+import javax.swing.*;
+
+public class Producer extends JFrame{
+    private JButton addNewMachineButton;
+    private JComboBox comboBox1;
+    private JButton viewMachineButton;
+    private JButton removeMachineButton;
+    private JPanel panel1;
+
+    public Producer(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.pack();
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new Producer("Producer");
+        frame.setVisible(true);
+    }
 }
