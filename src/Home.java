6675ebbf7c3ad1418c3272dff3829c06a677f95b
sheryar janjua <u1654095@unimail.co.uk>
2021-08-12 10:03:51 +0100

    Buttons to open producer and Packager forms

--- src/Home.java
+++ src/Home.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Home extends JFrame {
     private JPanel panel1;
     private JButton producerMachinesButton;
     private JLabel hopperCountNumField;
+    private JButton packagingMachinesButton;
+
 
     public Home(String title){
         super(title);
@@ -11,6 +15,20 @@ public class Home extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(panel1);
         this.pack();
+        producerMachinesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame2 = new Producer("Producer");
+                frame2.setVisible(true);
+            }
+        });
+        packagingMachinesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame3 = new Packager("Packager");
+                frame3.setVisible(true);
+            }
+        });
     }
 
     public static void main(String[] args) {
