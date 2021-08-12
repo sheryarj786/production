ce39adeb46840c6c0f2b4ad000ccffb18ff80e0f
sheryar janjua <u1654095@unimail.co.uk>
2021-08-12 14:58:22 +0100

    Fucntion for producer to make a new snack every 3 seconds when started

--- src/ProducerMachine.java
+++ src/ProducerMachine.java
@@ -1,18 +1,30 @@
-public class ProducerMachine {
-    int treats = 0;
+import java.util.*;
+
+public class ProducerMachine extends TimerTask{
+    int treat = 0;
     boolean started = false;
-    boolean paused = false;
+
+
 
     public void start() {
         started = true;
+        System.out.println("Status changed to started...");
+        System.out.println("The starting number of treats is: " + treat);
+        System.out.println("Every 3 seconds a new treat will be added");
+        Timer timer = new Timer();
+        timer.schedule(new ProducerMachine(), 0, 3000);
+
     }
 
-    public void stop(){
-        started = false;
-    }
-    public void pause(){
-        paused = true;
+    @Override
+    public void run() {
+        treat ++;
+        System.out.println("There is now " + treat + " treats");
+
     }
 
-
+    public static void main(String[] args) {
+        ProducerMachine producerMachine = new ProducerMachine();
+        producerMachine.start();
+    }
 }
